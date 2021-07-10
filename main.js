d1b4ceda0e7a88b684e30863d2eb52b86c16f4b4
arnavkh21 <72095856+arnavkh21@users.noreply.github.com>
2021-07-10 16:20:17 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+function preload(){
+
+}
+
+function setup(){
+   canvas= createCanvas(300,300);
+   canvas.center();
+}
+
+function draw(){
+
+}
+function take_snapshot(){
+    save('deleto.jfif');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
+
+
+
