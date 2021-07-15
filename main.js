fcfd6f1d1346a9d512b5eb8cde3e65133f0d1554
arnavkh21 <72095856+arnavkh21@users.noreply.github.com>
2021-07-15 12:19:20 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-function preload(){
+noseX = 0;
+noseY = 0;
 
+
+function preload(){
+clown_nose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
 }
 
 function setup(){
@@ -16,6 +20,8 @@ function setup(){
 
 function draw(){
 image(video ,0,0,300,300);
+image(clown_nose,noseX,noseY,30,30);
+
 }
 function take_snapshot(){
     save('deleto.jpg');
@@ -71,4 +77,13 @@ if(results.length > 0){
     console.log("nose x =" + results[0].pose.nose.x);
     console.log("nose y= " + results[0].pose.nose.y);
 }
+}function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
 }
+
